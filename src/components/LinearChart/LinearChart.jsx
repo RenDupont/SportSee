97946d3f00a2aecd51f63be3b5a3d7485cc4f088
Dupont <zeshin59@gmail.com>
2023-10-27 00:05:24 +0200

    LinearChart, work in progress

--- src/components/LinearChart/LinearChart.jsx
+++ src/components/LinearChart/LinearChart.jsx
@@ -1,23 +1,25 @@
 import Classes from './LinearChart.module.css';
 import { 
     LineChart, Line, XAxis,
-    YAxis, CartesianGrid, Tooltip, 
-    Legend, ResponsiveContainer 
+    Tooltip, Legend, ResponsiveContainer 
 } from 'recharts';
 
 function LinearChart({data}) {
-    return (
-        <ResponsiveContainer width="25%" height={263}>
-          <LineChart data={data.sessions}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="sessionLength" stroke="#8884d8" activeDot={{ r: 8 }} />
-          </LineChart>
-        </ResponsiveContainer>
-      );
+
+  const customFormat = (value) => {
+    return <span style={{ color: 'black' }}>{value} min</span>;
+  };
+
+  return (
+    <ResponsiveContainer width="25%" height={263}>
+      <LineChart data={data.sessions} style={{ backgroundColor: "#FF0000" }}>
+        <XAxis dataKey="day" tickFormatter={(value) => value} />
+        <Tooltip formatter={(value) => customFormat(value)} />
+        <Legend />
+        <Line type="monotone" dataKey="sessionLength" stroke="#FFFFFF" dot={false} />
+      </LineChart>
+    </ResponsiveContainer>
+  );
 }
 
 export default LinearChart;
